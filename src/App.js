2677fe738d7ed9b767d6d74e10621525eb5f25e0
varunkumarmedam <varunkumarmedam@gmail.com>
2023-12-31 21:41:10 +0530

    showing static coins data using a selectable dropdown list component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import CoinsDropdown from './components/CoinsDropdown';
+import { useState } from 'react';
 
 function App() {
+  const [selectedCoin, setSelectedCoin] = useState({});
+
+  const [coinsList, setCoinsList] = useState([{
+    "id": "bitcoin",
+    "name": "Bitcoin",
+    "symbol": "btc",
+    "logo": "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1696501400"
+  },
+  {
+    "id": "ethereum",
+    "name": "Ethereum",
+    "symbol": "eth",
+    "logo": "https://assets.coingecko.com/coins/images/279/large/ethereum.png?1696501628"
+  },
+  {
+    "id": "tether",
+    "name": "Tether",
+    "symbol": "usdt",
+    "logo": "https://assets.coingecko.com/coins/images/325/large/Tether.png?1696501661"
+  },
+  {
+    "id": "binancecoin",
+    "name": "BNB",
+    "symbol": "bnb",
+    "logo": "https://assets.coingecko.com/coins/images/825/large/bnb-icon2_2x.png?1696501970"
+  }]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CoinsDropdown
+        handleChange={(val) => { setSelectedCoin(val) }}
+        selectedVal={selectedCoin?.name}
+        options={coinsList} />
     </div>
   );
 }
